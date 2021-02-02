2ae9c82f07cfb564e80c2728735eb78f02718110
Johann <johann.ulma@gmail.com>
2021-02-02 17:31:33 +0100

    Initial Commit
    First changes in App
    Remodel to prepare MVC Model
    Adding tests &  Swiftlint configuration
    And Readme files

---
+++ CountOnMe/Model/Calculator.swift
@@ -0,0 +1,13 @@
+//
+//  Calculator.swift
+//  CountOnMe
+//
+//  Created by Johann on 02/02/2021.
+//  Copyright © 2021 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+
+class Calculator {
+    
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# CountOnMe
+
+# Project 5 OpenClassrooms : iOS Student
+
+_Soft Basics : _
+
+CountOnMe it’s a Calculator App.
